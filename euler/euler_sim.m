846e993061d8af354b1da3d777544145bb30abd0
Joshua <joshua.kashambala@gmail.com>
2026-09-22 21:01:07 +0000

    back to regular mass damper system

--- euler/euler_sim.m
+++ euler/euler_sim.m
@@ -25,7 +25,7 @@ equil_pos = u/k;
 for num = index
     y_values(num) = position;
 
-    acceleration = (u - (b*velocity) - (k*position*0))/m;
+    acceleration = (u - (b*velocity) - (k*position))/m;
 
     velocity = velocity + acceleration * dt;
     position = position + velocity * dt;
